f63b393403988c7c7e3f33d39b696e4da7bb3d3d
Zivile V. Silveira <54817312+Zivilevs@users.noreply.github.com>
2022-09-01 10:16:21 -0300

    Add files via upload

--- multi_configs_from_file.sh
+++ multi_configs_from_file.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
 
-read -p "Please enter the path for config list file: " fpath
+read -p "Please enter the relative path for config list file: " fpath
 
 FILE=$fpath
+
+read -p "How many minutes to wait between next job request?" twait
 
 until [ -f "$FILE" ]; do
     read -p "$FILE path does not exist. Please enter filepath: " fpath
     FILE=$fpath
 done
-echo "$FILE exists.i"
-echo "Initiating the job..."
+echo "$FILE exists."
 
+echo "Initiating the job..."
 
 while IFS= read -r line; do
         echo "Config $line is submitted"
-	    SKAPHOS_ENV=ECS skaphos RawToStacked --config-path skaphos/config/raw/"$line" &
+	    SKAPHOS_ENV=ECS skaphos RawToStacked --config-path "$line" &
 		BACK_PID=$!
 		wait $BACK_PID
-        sleep 1m
+        sleep "$twait"m
 done < "$FILE"
 
 echo "JOB COMPLETED"
